7c15a376f32510fc22299d3bad75247ccfa2cea1
EneaSema <sema.enea1993@gmail.com>
2025-05-12 18:13:44 +0200

    aggiornati-img

--- index.js
+++ index.js
@@ -16,33 +16,33 @@ app.get("/bacheca", (req, res) => {
   const posts = [
     {
       title: "title-1",
-      text: "description",
-      img: "/images/ciambellone.jpeg",
-      tags: ["tag-1", "tag-2", "tag-3", "tag-4", "tag-5"],
+      text: "Simbolo di fierezza è nobiltà",
+      img: "/images/lupo.jpg",
+      tags: ["animale", "leader", "fierezza", "nobiltà", "orgoglio"],
     },
     {
       title: "title-2",
-      text: "description",
-      img: "/images/cracker_barbabietola.jpeg",
-      tags: ["tag-1", "tag-2", "tag-3", "tag-4", "tag-5"],
+      text: "Strumento per poter imparare gratuitamente",
+      img: "/images/youtube.jpg",
+      tags: ["tutorial", "gratis", "imparare", "migliorare", "formazione"],
     },
     {
       title: "title-3",
-      text: "description",
-      img: "/images/pane_fritto_dolce.jpeg",
-      tags: ["tag-1", "tag-2", "tag-3", "tag-4", "tag-5"],
+      text: "Passione Manga ed Anime",
+      img: "/images/escanor.jpg",
+      tags: ["manga", "anime", "svago", "passatempo", "fantasticare"],
     },
     {
       title: "title-4",
-      text: "description",
-      img: "/images/pasta_barbabietola.jpeg",
-      tags: ["tag-1", "tag-2", "tag-3", "tag-4", "tag-5"],
+      text: "Danza Latino Americano",
+      img: "/images/bachata.jpg",
+      tags: ["danza", "musica", "energia", "piacere", "divertimento"],
     },
     {
       title: "title-",
-      text: "description",
-      img: "/images/torta_paesana.jpeg",
-      tags: ["tag-1", "tag-2", "tag-3", "tag-4", "tag-5"],
+      text: "Squadra del cuore",
+      img: "/images/ac-milan-logo.jpg",
+      tags: ["calcio", "rossonero", "il diavolo", "milano", "champions league"],
     },
   ];
 
